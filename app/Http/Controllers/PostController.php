74e95c00e3bac9f7161fd9ad387d3434b2925272
tjahajabarusbsd <tjahajabarubsd@gmail.com>
2025-12-05 11:09:24 +0700

    Refactor PostController to support category listing and nested post detail
    - Add category() method
    - Adjust index() and show() to use new URL structure

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Category;
 use App\Models\Post;
 use Illuminate\Support\Facades\View;
 
@@ -13,9 +14,27 @@ class PostController extends Controller
         return view('post.index', compact('posts'));
     }
 
-    public function show($slug)
+    public function category($categorySlug)
     {
-        $post = Post::where('slug', $slug)->firstOrFail();
-        return view('post.show', compact('post'));
+        $category = Category::where('slug', $categorySlug)->firstOrFail();
+
+        $posts = $category->posts()
+            ->where('is_published', true)
+            ->latest()
+            ->paginate(10);
+
+        return view('post.category', compact('category', 'posts'));
     }
+
+    public function show($categorySlug, $postSlug)
+    {
+        $category = Category::where('slug', $categorySlug)->firstOrFail();
+
+        $post = Post::where('slug', $postSlug)
+            ->where('category_id', $category->id) // pastikan slug post sesuai kategorinya
+            ->firstOrFail();
+
+        return view('post.show', compact('category', 'post'));
+    }
+
 }
